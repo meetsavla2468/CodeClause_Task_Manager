098a6a37f49efb0f401154e1a858db502ce0f7e1
meetsavla4444 <lcs2021010@iiitl.ac.in>
2023-07-21 20:59:36 +0530

    adding riverpod code

---
+++ lib/codeProvider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'codeProvider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$codeStateHash() => r'633477d42b681196934f4cba9c4dd7dd1d353e08';
+
+/// See also [codeState].
+@ProviderFor(codeState)
+final codeStateProvider =
+    AutoDisposeNotifierProvider<codeState, String>.internal(
+  codeState.new,
+  name: r'codeStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$codeStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$codeState = AutoDisposeNotifier<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
