b46ca9d2774fc162c1e5536b327b8765f7364839
meetsavla4444 <lcs2021010@iiitl.ac.in>
2023-07-25 20:24:19 +0530

    adding providers

---
+++ lib/providers/datesProvider.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'datesProvider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$datesStateHash() => r'da9b068946f895950d232fb24b8da7c59d0b1063';
+
+/// See also [datesState].
+@ProviderFor(datesState)
+final datesStateProvider =
+    AutoDisposeNotifierProvider<datesState, String>.internal(
+  datesState.new,
+  name: r'datesStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$datesStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$datesState = AutoDisposeNotifier<String>;
+String _$startTimeStateHash() => r'af04d90d44f1167e659114402ea2e7cd877bcfa3';
+
+/// See also [startTimeState].
+@ProviderFor(startTimeState)
+final startTimeStateProvider =
+    AutoDisposeNotifierProvider<startTimeState, String>.internal(
+  startTimeState.new,
+  name: r'startTimeStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$startTimeStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$startTimeState = AutoDisposeNotifier<String>;
+String _$finishTimeStateHash() => r'b69534f55fa6f1c2c9a0ab25b098df5b471f84ae';
+
+/// See also [finishTimeState].
+@ProviderFor(finishTimeState)
+final finishTimeStateProvider =
+    AutoDisposeNotifierProvider<finishTimeState, String>.internal(
+  finishTimeState.new,
+  name: r'finishTimeStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$finishTimeStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$finishTimeState = AutoDisposeNotifier<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
