fbb5fc7493d0b32c28e3ba8b6431d1f78e8d5d6d
meetsavla4444 <lcs2021010@iiitl.ac.in>
2023-07-25 11:15:45 +0530

    adding providers

---
+++ lib/providers/expansionProvider.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'expansionProvider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$expansionStateHash() => r'd081f6256f3552a1b53d51d99ddbbc14d79389df';
+
+/// See also [expansionState].
+@ProviderFor(expansionState)
+final expansionStateProvider =
+    AutoDisposeNotifierProvider<expansionState, bool>.internal(
+  expansionState.new,
+  name: r'expansionStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$expansionStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$expansionState = AutoDisposeNotifier<bool>;
+String _$expansionState0Hash() => r'88750b7b96406f5401a74aa8aae54bccd88e2484';
+
+/// See also [expansionState0].
+@ProviderFor(expansionState0)
+final expansionState0Provider =
+    AutoDisposeNotifierProvider<expansionState0, bool>.internal(
+  expansionState0.new,
+  name: r'expansionState0Provider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$expansionState0Hash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$expansionState0 = AutoDisposeNotifier<bool>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
---
+++ lib/providers/todoProvider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todoProvider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$todoStateHash() => r'52f525f325dd06dc42334217e646b408b42a7b47';
+
+/// See also [todoState].
+@ProviderFor(todoState)
+final todoStateProvider =
+    AutoDisposeNotifierProvider<todoState, List<TaskModel>>.internal(
+  todoState.new,
+  name: r'todoStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todoStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$todoState = AutoDisposeNotifier<List<TaskModel>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
